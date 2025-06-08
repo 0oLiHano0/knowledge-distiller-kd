251be6c6e6ce608e697639b88b044e67d25c83fa
Hansen <hansenli@outlook.com>
2025-06-08 23:18:59 +0800

    remove pytest-asyncio

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,3 @@ from tests.logging.dummy_logger import MockLogger
 def dummy_logger() -> LoggerProtocol:
     """返回一个符合 LoggerProtocol 的 mock logger 实例"""
     return MockLogger()
-
-def pytest_configure(config):
-    config.option.asyncio_mode = "auto"
-    config.option.asyncio_default_fixture_loop_scope = "function"
